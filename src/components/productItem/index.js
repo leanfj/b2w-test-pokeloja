8f590c8ad1244e28ec9bee0cd2fbcaf9c82b858a
leanfj <leanfj@hotmail.com>
2021-05-24 12:27:10 -0300

    Atualização de pacotes e Correção de captalize de nome de item

--- src/components/productItem/index.js
+++ src/components/productItem/index.js
@@ -7,7 +7,7 @@ const ProductItem = ({ name, sprite, price, handleCart }) => {
   return (
     <div className="item-container">
       <img src={sprite} alt="Pokemon" />
-      <p>{name}</p>
+      <p className="item-name">{name}</p>
       <p>{formatCurrency(price)}</p>
       <button
         className="poke-button"
